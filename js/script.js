a5aefa873f1b9689c4efe500a7f5191d1d7afe4d
tombyrnefp <tom.byrne@getfutureproof.co.uk>
2023-09-11 15:02:06 +0100

    fetching data

--- js/script.js
+++ js/script.js
@@ -2,12 +2,36 @@ const fruitForm = document.querySelector("#inputSection form")
 
 const fruitList = document.querySelector("#fruitSection ul")
 
+const fruitNutrition = document.querySelector("#nutritionSection p")
+
+const totals =document.querySelectorAll(".tol")
+
+const fetchFruitData = fruit => {
+    fetch(`https://fruity-api.onrender.com/api/fruits/${fruit}`)
+    .then(resp => resp.json())
+    .then(data => { 
+        console.log(data)
+        addFruit(data)
+    })
+    .catch(e => console.error(e))
+}
+
+let total = [0,0,0,0,0]
+
 const addFruit = fruit => {
+    
     const li = document.createElement("li")
-    li.textContent = fruit
+    li.textContent = fruit.name
     li.addEventListener("click", removeFruit, {once:true})
     fruitList.appendChild(li)
+    total[0] += fruit.nutritions.calories
+    total[1] += fruit.nutritions.carbohydrates
+    total[2]  += fruit.nutritions.protein
+    total[3]  += fruit.nutritions.fat
+    total[4] += fruit.nutritions.sugar
+    total.forEach((value, index) => totals[index].textContent = value.toFixed(2))
 }
+
 
 const removeFruit = e => {
     e.target.remove()
@@ -15,7 +39,7 @@ const removeFruit = e => {
 
 fruitForm.addEventListener("submit", e => {
     e.preventDefault()
-    addFruit(e.target.fruitInput.value)
+    fetchFruitData(e.target.fruitInput.value)
     e.target.fruitInput.value = ""
 })
 
