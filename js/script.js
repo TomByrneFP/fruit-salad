05b0e6d42e41fcf1483f4f704b1d288e3583f401
tombyrnefp <tom.byrne@getfutureproof.co.uk>
2023-09-11 12:06:38 +0100

    Reaady to use APIs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 const fruitForm = document.querySelector("#inputSection form")
 
-console.log(fruitForm)
+const fruitList = document.querySelector("#fruitSection ul")
+
+const addFruit = fruit => {
+    const li = document.createElement("li")
+    li.textContent = fruit
+    li.addEventListener("click", removeFruit, {once:true})
+    fruitList.appendChild(li)
+}
+
+const removeFruit = e => {
+    e.target.remove()
+}
+
+fruitForm.addEventListener("submit", e => {
+    e.preventDefault()
+    addFruit(e.target.fruitInput.value)
+    e.target.fruitInput.value = ""
+})
+
+
+
